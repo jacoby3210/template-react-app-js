9640bcdbd543e6dacf1853bb90e03fec36d8f5af
Iacovlev Alexander <jacoby3210@gmail.com>
2024-12-12 01:15:53 +0200

    update: file and folder structure.

--- src/project/sys/main.jsx
+++ src/project/sys/main.jsx
@@ -6,15 +6,17 @@ import { App }          from './App';
 // Create and setup React Web Application.
 // ------------------------------------------------------------------------- //
 
-async function enableMocking() {
-	if (process.env.NODE_ENV === "development") {
-		const { worker } = await import("/src/code/development/mocks/index.js");    
-		return worker.start();  
-	}
-}
+// async function enableMocking() {
+// 	if (process.env.NODE_ENV === "development") {
+// 		const { worker } = await import("/src/code/development/mocks/index.js");    
+// 		return worker.start();  
+// 	}
+// }
 
-console.log("Start Application")
+// console.log("Start Application")
 const root = ReactDOM.createRoot(document.getElementById('root'));
-enableMocking().then(() => {root.render(<App/>);});
+// enableMocking().then(() => {
+  root.render(<App/>);
+// });
 
 // ------------------------------------------------------------------------- //
